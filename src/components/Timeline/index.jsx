f4f6774d88ee097efe100d6df4324663cd1b40d1
Zuka-Dev <danazuka2003@gmail.com>
2023-09-23 22:11:37 +0100

    Timeline edit change

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -6,9 +6,12 @@ import "react-vertical-timeline-component/style.min.css";
 import { timelineData } from "./data";
 const Timeline = () => {
   return (
-    <div className="flex flex-col items-center gap-4 lg:mb-[245px]" id="timeline">
+    <div
+      className="flex flex-col items-center gap-4 lg:mb-[245px]"
+      id="timeline"
+    >
       <p className="font1 text-white text-3xl font-bold">Timeline</p>
-      <p className="font2 text-white text-sm mb-5 text-center">
+      <p className="font2 text-white text-sm mb-5 text-center px-7 lg:px-0">
         Here is the breakdown of the time we anticipate using for the upcoming
         event.
       </p>
